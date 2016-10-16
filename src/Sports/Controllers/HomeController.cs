40d821c1cc3c4b15dfaa8078260b2a34d02ef4ce
knnbayaz <knnbayaz@gmail.com>
2016-10-17 00:14:14 +0300

    First Initial

---
+++ src/Sports/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sports.Models;
+
+namespace Sports.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+
+        [Produces("application/json")]
+        public IActionResult BaseApi()
+        {
+            TodoItem todo = new TodoItem();
+
+            todo.TaskName = "Sample";
+            todo.TaskDefiniton = "Sample Definiton";
+            todo.DueDate = new DateTime(2016, 10, 17);
+
+            return Json(todo);
+        }
+    }
+}
---
+++ src/Sports/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sports.Models
+{
+    public class TodoItem
+    {
+        public string TaskName { get; set; }
+
+        public string TaskDefiniton { get; set; }
+
+        public DateTime DueDate { get; set; }
+    }
+}
